f38ae5603cd977328f36e8b916c430e369d9d5d0
David H. Gutteridge <dhgutteridge@users.noreply.github.com>
2023-06-12 19:47:56 -0400

    Include Python 3.11 in supported list

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries :: Python Modules",
